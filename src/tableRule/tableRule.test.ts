4fb2cf50413d48bb430e42a35f75eedfbe65bf91
garrygu <garry.guzy@gmail.com>
2020-02-14 18:29:14 +0800

    test: update test

--- src/tableRule/tableRule.test.ts
+++ src/tableRule/tableRule.test.ts
@@ -3,9 +3,12 @@ import {
   initializeRender,
   selectRuleType,
   selectRuleNum,
-  getRuleResult
+  getRuleResult,
+  bindSelect
 } from "./tableRule";
 import "@testing-library/jest-dom";
+
+global.$ = require("jquery");
 
 describe("正确显示两个select以及其下的options", () => {
   beforeEach(() => {
@@ -14,6 +17,7 @@ describe("正确显示两个select以及其下的options", () => {
       <select id="rule_num"></select>
     `;
     initializeRender();
+    bindSelect();
   });
 
   it(`如果rule_type是"不限"，则rule_num应该是hidden，并且选择到1`, () => {
@@ -40,24 +44,39 @@ describe("正确显示两个select以及其下的options", () => {
 
 describe("在使用上的交互", () => {
   it(`如果选择的不是"不限", 则rule_num下拉选单取消hidden状态，并且可以进行options的选择`, () => {
-    selectRuleType(1);
+    const ruleTypeEl = window.document.getElementById("rule_type");
+    $(ruleTypeEl!)
+      .val(1)
+      .change();
     const ruleNumSelector = window.document.getElementById("rule_num");
     const ruleNumSelectorChildren = ruleNumSelector!.children;
     expect(ruleNumSelector).toBeVisible();
     expect(ruleNumSelectorChildren.length).toBe(10); // 1~10
   });
   it(`非不限的情况下，选择rule_num下拉框，选择一个值，则得到这个值的结果`, () => {
-    selectRuleType(2);
+    const ruleTypeEl = window.document.getElementById("rule_type");
+    $(ruleTypeEl!)
+      .val(2)
+      .change();
     const ruleNumSelector = window.document.getElementById("rule_num");
     expect(ruleNumSelector).toBeVisible();
-    selectRuleNum(2);
+    $(ruleNumSelector!)
+      .val(2)
+      .change();
     const result = getRuleResult();
     expect(result.rule_num).toBe(2);
   });
   it(`如果选择的是"不限"，则rule_num下拉框进入到disable状态, 得到的结果rule_type 为0 ， rule_num为1`, () => {
-    selectRuleNum(10);
-    selectRuleType(0);
     const ruleNumSelector = window.document.getElementById("rule_num");
+    expect(ruleNumSelector).toBeVisible();
+    $(ruleNumSelector!)
+      .val(10)
+      .change();
+    const ruleTypeEl = window.document.getElementById("rule_type");
+    $(ruleTypeEl!)
+      .val(0)
+      .change();
+
     expect(ruleNumSelector).not.toBeVisible();
     const { rule_type, rule_num } = getRuleResult();
     expect(rule_type).toBe(0);
